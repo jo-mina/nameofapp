f711a874b75ac769b5c03704bfd75d289aa87609
jo-mina <joane.mina@outlook.com>
2017-01-25 20:12:52 +0000

    added email funtionality in both development and production with sendgrid plugin

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,6 +6,17 @@ class StaticPagesController < ApplicationController
   	def landing_page
 		@products = Product.limit(4)
 	end
+
+	def thank_you
+		@name = params[:name]
+		@email = params[:email]
+		@message = params[:message]
+		ActionMailer::Base.mail(:from => @email,
+			:to => 'iclejo2k8@yahoo.co.uk',
+			:subject => "A new contact form message from #{@name}",
+			:body => @message).deliver_now
+	end
+
 end
 
 

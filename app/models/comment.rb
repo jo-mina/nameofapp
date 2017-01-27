9868ca5a4f9640118589e5b52e7c63b8195bd35e
jo-mina <joane.mina@outlook.com>
2017-01-27 22:32:32 +0000

    5.7 created scope for lowest rated product and added custom method for lowest rated product

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,8 @@ class Comment < ApplicationRecord
 
   #scope which selects the comments with the highest rating
   scope :rating_desc, -> {order(rating: :desc)}
+
+  #scope which selects the comments with the lowest rating
+  scope :rating_asc, -> {order(rating: :asc)}
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,9 @@ class Product < ApplicationRecord
   def highest_rating_comment
   	comments.rating_desc.first
   end
+
+   #custom method for our product to return the comment with the lowest rating
+  def lowest_rating_comment
+  	comments.rating_asc.first
+  end
 end

fb3bb2498e5f624b28df5e02ba500e560f87b92e
jo-mina <joane.mina@outlook.com>
2017-01-27 22:11:55 +0000

    5.7 added scope for highest rated comment and custom method for product to return highest rating

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  #scope which selects the comments with the highest rating
+  scope :rating_desc, -> {order(rating: :desc)}
 end

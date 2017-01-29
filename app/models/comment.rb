1a7c4503023178b15470a8ecd25e647e11009002
jo-mina <joane.mina@outlook.com>
2017-01-29 00:00:17 +0000

    5.9 added comment validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
+  #validations to ensure very comment record should be saved with a message, user id, product id and rating is present, where rating is an integer
+  validates :body, presence: true
+  validates :user, presence:true
+  validates :product, presence: true
+  validates :rating, numericality: {only_integer: true}
+
+  #asscociations with user and product model
   belongs_to :user
   belongs_to :product
 

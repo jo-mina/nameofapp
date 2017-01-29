5f63524e224487903d53e1b400cb7ae8d8b3af4f
jo-mina <joane.mina@outlook.com>
2017-01-29 17:47:40 +0000

    5.11 added confirmation sign up email functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :orders
+
+	#after create user send a confrimation email to their inbox
+	after_create :welcome_send
+	def welcome_send
+		WelcomeMailer.welcome_send(self).deliver
+	end
+
 end

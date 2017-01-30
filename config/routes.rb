991035e95c1d26e9e02e349ba81669b4a038e79f
jo-mina <joane.mina@outlook.com>
2017-01-30 20:21:30 +0000

    6.6 setup payment controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   	get 'static_pages/index'
   	get 'static_pages/landing_page'
   	post 'static_pages/thank_you'
+    post 'payments/create'
   	root 'static_pages#landing_page'
   	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

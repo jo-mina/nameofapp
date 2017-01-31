adf12cbcf9699026903d6ddbcd788eb337d32bed
jo-mina <joane.mina@outlook.com>
2017-01-31 16:48:49 +0000

    6.9 added jquery zoom elevate plugin

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,7 @@
 $(document).on('turbolinks:load', function(){
+	//jquery Zoom Plugin
+	$('.img-zoom').elevateZoom({tint:true, tintColour:'#df9fbf', tintOpacity:0.4});
+    //plugin for star rating 
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
       readOnly: true,

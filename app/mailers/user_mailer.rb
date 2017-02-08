156a239d9535e6bfb663d9d3f01f672aec3cf763
jo-mina <joane.mina@outlook.com>
2017-02-08 22:53:16 +0000

    6.15 fixed payment confirmation mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,7 +10,7 @@ class UserMailer < ApplicationMailer
 
 	def payment_confirmation(user)
 		@user = user
-		mail(:to => user,
+		mail(:to => user.email,
 			:from => "sales@bikesgalore.com",
 			:subject => "We Have Received Your Payment. Thank You.")
 	end

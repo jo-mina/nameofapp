c60d946f00537dfd7ded3808720782f8375cc20f
jo-mina <joane.mina@outlook.com>
2017-02-08 22:20:24 +0000

    6.15 made changes to stripe payment functionality

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,9 @@
 if Rails.env.production?
   Rails.configuration.stripe = {
-    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-    :secret_key => ENV['STRIPE_SECRET_KEY']
+    #:publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    #:secret_key => ENV['STRIPE_SECRET_KEY']
+    :publishable_key => 'pk_test_A3Wem64i7EFTWlCDrFPBcFav',
+    :secret_key => 'sk_test_RdjXiLiuLMKpHBVFgjY4z5z1'
   }
 else
   Rails.configuration.stripe = {
